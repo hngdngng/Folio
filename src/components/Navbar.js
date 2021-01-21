02cf9429a7d31f325835170ef8bcd20f1423ec5e
hngdng <65930682+hngdngng@users.noreply.github.com>
2021-01-21 11:36:15 -0500

    added scroll to top component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { AppBar, Container, Hidden, IconButton, List, ListItem, ListItemText, Toolbar } from "@material-ui/core";
-import { Home } from "@material-ui/icons";
+import { AppBar, Container, Fab, Hidden, IconButton, List, ListItem, ListItemText, Toolbar } from "@material-ui/core";
+import { Home, KeyboardArrowUp } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
 import SideDrawer from "./SideDrawer";
+import HideOnScroll from "./HideOnScroll";
+import BackToTop from "./BackToTop";
 
 const useStyles = makeStyles({
     navDisplayFlex: {
@@ -23,30 +25,41 @@ const navLinks = [
 
 const Navbar = () => {
     const classes = useStyles();
+
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <Container className={classes.navDisplayFlex}>
-                    <IconButton edge="start" color="inherit" aria-label="home">
-                        <Home fontSize="large" />
-                    </IconButton>
-                    <Hidden smDown>
-                        <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
-                            {navLinks.map(({ title, path }) => (
-                                <a href={path} key={title} className={classes.linkText}>
-                                    <ListItem button>
-                                        <ListItemText primary={title} />
-                                    </ListItem>
-                                </a>
-                            ))}
-                        </List>
-                    </Hidden>
-                    <Hidden mdUp>
-                        <SideDrawer navLinks={navLinks} />
-                    </Hidden>
-                </Container>
-            </Toolbar>
-        </AppBar>
+        <>
+            <HideOnScroll>
+                <AppBar position="fixed">
+                    <Toolbar>
+                        <Container className={classes.navDisplayFlex}>
+                            <IconButton edge="start" color="inherit" aria-label="home">
+                                <Home fontSize="large" />
+                            </IconButton>
+                            <Hidden smDown>
+                                <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
+                                    {navLinks.map(({ title, path }) => (
+                                        <a href={path} key={title} className={classes.linkText}>
+                                            <ListItem button>
+                                                <ListItemText primary={title} />
+                                            </ListItem>
+                                        </a>
+                                    ))}
+                                </List>
+                            </Hidden>
+                            <Hidden mdUp>
+                                <SideDrawer navLinks={navLinks} />
+                            </Hidden>
+                        </Container>
+                    </Toolbar>
+                </AppBar>
+            </HideOnScroll>
+            <Toolbar id="back-to-top-anchor" />
+            <BackToTop>
+                <Fab color="secondary" size="large" aria-label="scroll back to top">
+                    <KeyboardArrowUp />
+                </Fab>
+            </BackToTop>
+        </>
     );
 }
 
